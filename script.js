f7620dee70ddf2b67c09da6094ea71ac9d9eba45
Daniel ccnl <daniel.vanbavel@codecentric.nl>
2022-09-12 20:24:42 +0200

    added cloud test

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
-    vus: 1,
+    vus: 10,
     duration: '10s'
 }
 
