707e04cdba410c9ee8a3a5331a631f5453cf2316
Daniel ccnl <daniel.vanbavel@codecentric.nl>
2022-09-12 20:31:02 +0200

    Update project files

--- script-cloud.js
+++ script-cloud.js
@@ -2,8 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  duration: '1m',
-  vus: 50,
+    stages: [
+        { duration: '2m', target: 50 },
+        { duration: '4m', target: 50 },
+        { duration: '2m', target: 0 },
+    ],
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
@@ -11,6 +14,6 @@ export const options = {
 };
 
 export default function () {
-  const res = http.get('https://test.k6.io');
+  http.get('https://test.k6.io');
   sleep(1);
 }
