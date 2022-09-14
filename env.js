c8be0908796a6fca9ece9936b2378a0ed3cd75a6
Daniel ccnl <daniel.vanbavel@codecentric.nl>
2022-09-14 15:00:31 +0200

    changes

---
+++ env.js
@@ -0,0 +1,4 @@
+export let devEnvironment = "http://www.google.nl"
+
+export let TG1_VU=25
+export let TG1_DURATION="20s"
--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 import http from 'k6/http';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+import * as env from './env.js'
 
 export const options = {
-    vus: 10,
-    duration: '10s'
+    duration: `${env.TG1_DURATION}`,
+    vus: `${env.TG1_VU}`
 }
 
 export default function() {
-    http.get("http://10.0.0.75:8888/demo")
-    http.get("http://10.0.0.75:8888/load")
-}
-
-export function handleSummary(data) {
-    return {
-        "./reports/summary-TG1.html": htmlReport(data),
-    };
+    http.get(`${env.devEnvironment}`)    
 }
